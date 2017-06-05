fe316d2392875ef0b86d5559e45f9d4e039fc88a
liuqi0826 <liuqi0826@gmail.com>
2017-06-05 16:58:57 +0800

    Add box test.

--- geom/ray.go
+++ geom/ray.go
@@ -143,3 +143,230 @@ func (this *Ray) IntersectTriangleWithHitPoint(triangle *Triangle) *Vector4 {
 	point.Add(&this.Origin)
 	return &point
 }
+func (this *Ray) IntersectAABB(aabb *Box) bool {
+	testPlaneList := make([]*Plane, 0)
+	if this.Origin.X < aabb.Max.X && this.Origin.Y < aabb.Max.Y && this.Origin.Z < aabb.Max.Z && this.Origin.X > aabb.Mix.X && this.Origin.Y > aabb.Mix.Y && this.Origin.Z > aabb.Mix.Z {
+		return true
+	}
+	if this.Origin.X > aabb.Max.X {
+		p0 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+		p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p2 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	if this.Origin.Y > aabb.Max.Y {
+		p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+		p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+		p2 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	if this.Origin.Z > aabb.Max.Z {
+		p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+		p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p2 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	if this.Origin.X < aabb.Max.X {
+		p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+		p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+		p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	if this.Origin.Y < aabb.Max.Y {
+		p0 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+		p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	if this.Origin.Z < aabb.Max.Z {
+		p0 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+		p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+		p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+		p := new(Plane)
+		p.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, p)
+	}
+	for _, p := range testPlaneList {
+		tp := this.IntersectPlaneHitPoint(p)
+		if tp != nil {
+			if tp.X-aabb.Max.X >= 0 && tp.X-aabb.Max.X < 0.00000001 {
+				if tp.Y >= aabb.Mix.X && tp.Y <= aabb.Max.Y && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					return true
+				}
+			}
+			if tp.Y-aabb.Max.Y >= 0 && tp.Y-aabb.Max.Y < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					return true
+				}
+			}
+			if tp.Z-aabb.Max.Z >= 0 && tp.Z-aabb.Max.Z < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Y >= aabb.Mix.Y && tp.Y <= aabb.Max.Y {
+					return true
+				}
+			}
+			if tp.X-aabb.Mix.X >= 0 && tp.X-aabb.Mix.X < 0.00000001 {
+				if tp.Y >= aabb.Mix.X && tp.Y <= aabb.Max.Y && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					return true
+				}
+			}
+			if tp.Y-aabb.Mix.Y >= 0 && tp.Y-aabb.Mix.Y < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					return true
+				}
+			}
+			if tp.Z-aabb.Mix.Z >= 0 && tp.Z-aabb.Mix.Z < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Y >= aabb.Mix.Y && tp.Y <= aabb.Max.Y {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+func (this *Ray) IntersectAABBWithHitPoint(aabb *Box) *Vector4 {
+	testPlaneList := make([]*Plane, 0)
+	if this.Origin.X < aabb.Max.X && this.Origin.Y < aabb.Max.Y && this.Origin.Z < aabb.Max.Z && this.Origin.X > aabb.Mix.X && this.Origin.Y > aabb.Mix.Y && this.Origin.Z > aabb.Mix.Z {
+		var p0, p1, p2 *Vector4
+		p0.X, p0.Y, p0.Z = aabb.Max.X, aabb.Max.Y, aabb.Max.Z
+		p1.X, p1.Y, p1.Z = aabb.Max.X, aabb.Mix.Y, aabb.Max.Z
+		p2.X, p2.Y, p2.Z = aabb.Max.X, aabb.Mix.Y, aabb.Mix.Z
+		pl0 := new(Plane)
+		pl0.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl0)
+		p0.X, p0.Y, p0.Z = aabb.Mix.X, aabb.Max.Y, aabb.Mix.Z
+		p1.X, p1.Y, p1.Z = aabb.Mix.X, aabb.Max.Y, aabb.Max.Z
+		p2.X, p2.Y, p2.Z = aabb.Max.X, aabb.Max.Y, aabb.Max.Z
+		pl1 := new(Plane)
+		pl1.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl1)
+		p0.X, p0.Y, p0.Z = aabb.Mix.X, aabb.Max.Y, aabb.Max.Z
+		p1.X, p1.Y, p1.Z = aabb.Mix.X, aabb.Mix.Y, aabb.Max.Z
+		p2.X, p2.Y, p2.Z = aabb.Max.X, aabb.Mix.Y, aabb.Max.Z
+		pl2 := new(Plane)
+		pl2.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl2)
+		p0.X, p0.Y, p0.Z = aabb.Mix.X, aabb.Max.Y, aabb.Mix.Z
+		p1.X, p1.Y, p1.Z = aabb.Mix.X, aabb.Mix.Y, aabb.Mix.Z
+		p2.X, p2.Y, p2.Z = aabb.Mix.X, aabb.Mix.Y, aabb.Max.Z
+		pl3 := new(Plane)
+		pl3.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl3)
+		p0.X, p0.Y, p0.Z = aabb.Max.X, aabb.Mix.Y, aabb.Mix.Z
+		p1.X, p1.Y, p1.Z = aabb.Max.X, aabb.Mix.Y, aabb.Max.Z
+		p2.X, p2.Y, p2.Z = aabb.Mix.X, aabb.Mix.Y, aabb.Max.Z
+		pl4 := new(Plane)
+		pl4.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl4)
+		p0.X, p0.Y, p0.Z = aabb.Max.X, aabb.Max.Y, aabb.Mix.Z
+		p1.X, p1.Y, p1.Z = aabb.Max.X, aabb.Mix.Y, aabb.Mix.Z
+		p2.X, p2.Y, p2.Z = aabb.Mix.X, aabb.Mix.Y, aabb.Mix.Z
+		pl5 := new(Plane)
+		pl5.FromPoints(p0, p1, p2)
+		testPlaneList = append(testPlaneList, pl5)
+	} else {
+		if this.Origin.X > aabb.Max.X {
+			p0 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+			p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p2 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+		if this.Origin.Y > aabb.Max.Y {
+			p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+			p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+			p2 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+		if this.Origin.Z > aabb.Max.Z {
+			p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Max.Z}
+			p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p2 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+		if this.Origin.X < aabb.Max.X {
+			p0 := &Vector4{X: aabb.Mix.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+			p1 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+			p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+		if this.Origin.Y < aabb.Max.Y {
+			p0 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+			p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Max.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+		if this.Origin.Z < aabb.Max.Z {
+			p0 := &Vector4{X: aabb.Max.X, Y: aabb.Max.Y, Z: aabb.Mix.Z}
+			p1 := &Vector4{X: aabb.Max.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+			p2 := &Vector4{X: aabb.Mix.X, Y: aabb.Mix.Y, Z: aabb.Mix.Z}
+			p := new(Plane)
+			p.FromPoints(p0, p1, p2)
+			testPlaneList = append(testPlaneList, p)
+		}
+	}
+	hitPointList := make([]*Vector4, 0)
+	for _, p := range testPlaneList {
+		tp := this.IntersectPlaneHitPoint(p)
+		if tp != nil {
+			if tp.X-aabb.Max.X >= 0 && tp.X-aabb.Max.X < 0.00000001 {
+				if tp.Y >= aabb.Mix.X && tp.Y <= aabb.Max.Y && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+			if tp.Y-aabb.Max.Y >= 0 && tp.Y-aabb.Max.Y < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+			if tp.Z-aabb.Max.Z >= 0 && tp.Z-aabb.Max.Z < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Y >= aabb.Mix.Y && tp.Y <= aabb.Max.Y {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+			if tp.X-aabb.Mix.X >= 0 && tp.X-aabb.Mix.X < 0.00000001 {
+				if tp.Y >= aabb.Mix.X && tp.Y <= aabb.Max.Y && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+			if tp.Y-aabb.Mix.Y >= 0 && tp.Y-aabb.Mix.Y < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Z >= aabb.Mix.Z && tp.Z <= aabb.Max.Z {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+			if tp.Z-aabb.Mix.Z >= 0 && tp.Z-aabb.Mix.Z < 0.00000001 {
+				if tp.X >= aabb.Mix.X && tp.X <= aabb.Max.X && tp.Y >= aabb.Mix.Y && tp.Y <= aabb.Max.Y {
+					hitPointList = append(hitPointList, tp)
+				}
+			}
+		}
+	}
+	if len(hitPointList) > 0 {
+		hit := &Vector4{X: 999999999, Y: 999999999, Z: 999999999}
+		for _, p := range hitPointList {
+			if hit.LengthSquared() < p.LengthSquared() {
+				hit = p
+			}
+		}
+		return hit
+	}
+	return nil
+}
