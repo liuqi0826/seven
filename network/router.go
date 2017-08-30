46ef01226ec04bdf53e60ced4edb206df8e67a35
liuqi0826 <liuqi0826@gmail.com>
2017-08-30 18:52:26 +0800

    ...

--- network/router.go
+++ network/router.go
@@ -4,19 +4,30 @@ import (
 	"errors"
 	"fmt"
 	"sync"
+	"time"
 )
+
+type CallBack struct {
+	CreateTime int64
+	Handle     func(*DataStruct)
+}
 
 type Route struct {
 	sync.Mutex
 
 	Default  func(*DataStruct)
-	Table    map[uint16]func(*DataStruct)
-	Callback map[uint16]func(*DataStruct)
+	Table    map[uint16]*CallBack
+	Callback map[uint16]*CallBack
+
+	ticker *time.Ticker
 }
 
 func (this *Route) Route() {
-	this.Table = make(map[uint16]func(*DataStruct))
-	this.Callback = make(map[uint16]func(*DataStruct))
+	this.Table = make(map[uint16]*CallBack)
+	this.Callback = make(map[uint16]*CallBack)
+
+	this.ticker = time.NewTicker(time.Minute * 10)
+	go this.check()
 }
 func (this *Route) Dispose() error {
 	var err error
@@ -42,7 +53,10 @@ func (this *Route) addHandle(title uint16, handle func(*DataStruct)) error {
 		if _, ok := this.Table[title]; ok {
 			err = errors.New("Route handle is not null")
 		} else {
-			this.Table[title] = handle
+			cb := new(CallBack)
+			cb.CreateTime = time.Now().Unix()
+			cb.Handle = handle
+			this.Table[title] = cb
 		}
 	} else {
 		err = errors.New("AddHandle function route is null")
@@ -72,7 +86,10 @@ func (this *Route) addCallback(index uint16, handle func(*DataStruct)) error {
 		if _, ok := this.Callback[index]; ok {
 			err = errors.New("Callback handle is not null")
 		} else {
-			this.Callback[index] = handle
+			cb := new(CallBack)
+			cb.CreateTime = time.Now().Unix()
+			cb.Handle = handle
+			this.Callback[index] = cb
 		}
 	} else {
 		err = errors.New("AddCallback function route is null")
@@ -81,13 +98,13 @@ func (this *Route) addCallback(index uint16, handle func(*DataStruct)) error {
 }
 func (this *Route) onData(ds *DataStruct) {
 	if this != nil {
-		if fun, ok := this.Callback[ds.ReceIndex]; ok {
-			go fun(ds)
+		if cb, ok := this.Callback[ds.ReceIndex]; ok {
+			go cb.Handle(ds)
 			delete(this.Callback, ds.ReceIndex)
 			return
 		}
-		if fun, ok := this.Table[ds.Title]; ok {
-			go fun(ds)
+		if cb, ok := this.Table[ds.Title]; ok {
+			go cb.Handle(ds)
 			return
 		} else {
 			if this.Default != nil {
@@ -95,6 +112,24 @@ func (this *Route) onData(ds *DataStruct) {
 			} else {
 				fmt.Println("Default route handle function is null")
 				fmt.Println("Route code is: " + fmt.Sprintf("%d", ds.Title))
+			}
+		}
+	}
+}
+func (this *Route) check() {
+	for this != nil {
+		select {
+		case <-this.ticker.C:
+			now := time.Now().Unix()
+			for k, v := range this.Table {
+				if now-v.CreateTime > 600 {
+					delete(this.Table, k)
+				}
+			}
+			for k, v := range this.Callback {
+				if now-v.CreateTime > 600 {
+					delete(this.Callback, k)
+				}
 			}
 		}
 	}
